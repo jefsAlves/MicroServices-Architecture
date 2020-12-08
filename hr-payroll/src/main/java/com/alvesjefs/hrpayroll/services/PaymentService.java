5f822d5a1dfb1e1be5ea9300e1fde4d1c7e4f29e
Jefferson Alves <jeffersonalmeida16@outlook.com>
2020-12-08 08:50:12 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/alvesjefs/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/alvesjefs/hrpayroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.alvesjefs.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.alvesjefs.hrpayroll.domain.Payment;
+import com.alvesjefs.hrpayroll.domain.Worker;
 
 @Service
 public class PaymentService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
 	public Payment instantiatePayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/api/workers/{id}", Worker.class, uriVariables);
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
